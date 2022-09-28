79199701f82a4d8aeef493665f6a0fdceac1bdc0
misael-mra <misaelopcao@gmail.com>
2022-09-28 13:33:09 -0300

    adicionada função de exclusão de bens

--- deletar_bens.php
+++ deletar_bens.php
@@ -4,18 +4,18 @@ require_once('includes/load.php');
 // Checkin What level user has permission to view this page
 page_require_level(1);
 
-$manufacturer = find_by_id('equipments',(int)$_POST['id']);
-if(!$manufacturer){
-	$session->msg("d","Equipamento não encontrado!");
-	redirect('equipamentos.php');
+$asset_del = find_by_id('assets',(int)$_POST['id']);
+if(!$asset_del){
+	$session->msg("d","Bem não encontrado!");
+	redirect('bens.php');
 }
 
-$delete_id = delete_by_id('equipments',(int)$manufacturer['id']);
+$delete_id = delete_by_id('assets',(int)$asset_del['id']);
 if($delete_id){
-	$session->msg("s","Equipamento excluído.");
-	redirect('equipamentos.php');
+	$session->msg("s","Bem excluído com sucesso!");
+	redirect('bens.php');
 } else {
-	$session->msg("d","Falha ao excluir o equipamento.");
-	redirect('equipamentos.php');
+	$session->msg("d","Falha ao excluir o bem.");
+	redirect('bens.php');
 }
 ?>
