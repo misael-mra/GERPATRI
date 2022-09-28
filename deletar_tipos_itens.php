e352591aa9441d98835af6cea88e9f7197c360b1
misael-mra <misaelopcao@gmail.com>
2022-09-28 16:27:37 -0300

    implementado função excluir da pagina de cadastro de tipos

--- deletar_tipos_itens.php
+++ deletar_tipos_itens.php
@@ -4,18 +4,18 @@ require_once('includes/load.php');
 // Checkin What level user has permission to view this page
 page_require_level(1);
 
-$sector = find_by_id('types_equips',(int)$_POST['id']);
+$sector = find_by_id('types_itens',(int)$_POST['id']);
 if(!$sector){
-  $session->msg("d","Tipo de equiapmento não encontrado!");
-  redirect('tipos_equipamento.php');
+  $session->msg("d","Tipo de item não encontrado!");
+  redirect('tipos_itens.php');
 }
 
-$delete_id = delete_by_id('types_equips',(int)$sector['id']);
+$delete_id = delete_by_id('types_itens',(int)$sector['id']);
 if($delete_id){
-  $session->msg("s","Tipo de equiapmento excluído.");
-  redirect('tipos_equipamento.php');
+  $session->msg("s","Item excluído.");
+  redirect('tipos_itens.php');
 } else {
-  $session->msg("d","Falha ao excluir o tipo de equiapmento.");
-  redirect('tipos_equipamento.php');
+  $session->msg("d","Falha ao excluir o item.");
+  redirect('tipos_itens.php');
 }
 ?>
