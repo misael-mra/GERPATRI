0ab0fb37e08b38d5d867c84033f3f42f38ea7dd8
unknown <Hgwa@NTI-015.hgwa.local>
2022-10-11 23:04:55 -0300

    Implementada a pagina de realizar baixa patrimonial

--- adicionar_baixa.php
+++ adicionar_baixa.php
@@ -1,13 +1,13 @@
 <?php
 require_once('includes/load.php');
 
-$page_title = 'Realizar transferência';
+$page_title = 'Realizar baixa';
 // Checkin What level user has permission to view this page
 page_require_level(2);
 ?>
 <?php
 
-if(isset($_POST['add_transfer'])){
+if(isset($_POST['add_asset_off'])){
   $req_fields = array('a_id','sector','responsible_user','transfer_date');
   if(empty($errors)){
     $a_id      = $db->escape((int) $_POST['a_id']);
@@ -17,22 +17,22 @@ if(isset($_POST['add_transfer'])){
     $a_user_create = (int) $_SESSION['user_id'];
     $a_date_create = make_date();
 
-    $sql  = "INSERT INTO transfers (";
+    $sql  = "INSERT INTO transfer_asset_off (";
     $sql .= " asset_id, responsible_user, sector_id, transfer_date, created_by, created_at";
     $sql .= ") VALUES (";
     $sql .= "'{$a_id}','{$a_r_u}','{$a_sector}','{$a_t_date}','{$a_user_create}','{$a_date_create}'";
     $sql .= ")";
 
     if($db->query($sql)){
-      $session->msg('s',"Transferência realizada com sucesso!");
-      redirect('adicionar_transferencia.php', false);
+      $session->msg('s',"Baixa patrimonial realizada com sucesso!");
+      redirect('adicionar_baixa.php', false);
     } else {
-      $session->msg('d','Desculpe, falha ao realizar o transferência');
-      redirect('adicionar_transferencia.php', false);
+      $session->msg('d','Desculpe, falha ao realizar a baixa patrimonial');
+      redirect('adicionar_baixa.php', false);
     }
   } else {
     $session->msg("d", $errors);
-    redirect('adicionar_transferencia.php',false);
+    redirect('adicionar_baixa.php',false);
   }
 }
 
@@ -60,22 +60,22 @@ if(isset($_POST['add_transfer'])){
     <div class="panel panel-default">
       <div class="panel-heading clearfix">
         <strong style="font-size:16px;">
-          <span class="glyphicon glyphicon-transfer"></span>
-          <span>Realizar transferência</span>
+          <span class="glyphicon glyphicon-download-alt"></span>
+          <span>Realizar baixa patrimonial</span>
         </strong>
         <div class="pull-right">
-          <a href="transferencias.php" class="btn btn-danger">cancelar</a>
+          <a href="bens_baixados.php" class="btn btn-danger">cancelar</a>
         </div>
       </div>
       <div class="panel-body">
-        <form method="post" action="adicionar_transferencia.php">
+        <form method="post" action="adicionar_baixa.php">
           <table class="table table-bordered">
             <thead>
               <th class="text-center"> Tombo</th>
               <th> Descrição do bem</th>
-              <th> Responsável pela Transferência</th>
-              <th> Setor de Destino</th>
-              <th class="text-center"> Data da transferência</th>
+              <th> Responsável pela Baixa</th>
+              <th> Unidade de destino</th>
+              <th class="text-center"> Data da Baixa</th>
               <th class="text-center"> Ação</th>
             </thead>
             <tbody  id="asset_info"> </tbody>
