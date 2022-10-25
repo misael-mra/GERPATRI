75369f4526d43150107d099fd36e6ea85f18d3cc
misael-mra <misaelopcao@gmail.com>
2022-10-25 16:12:07 -0300

    alteração tipos

--- editar_tipos_itens.php
+++ editar_tipos_itens.php
@@ -6,29 +6,29 @@ $page_title = 'Editar tipo de Item';
 page_require_level(1);
 
 //Display all types equipment.
-$type_equip = find_by_id('types_equips',(int)$_GET['id']);
-if(!$type_equip){
+$types_item = find_by_id('types_itens',(int)$_GET['id']);
+if(!$types_item){
   $session->msg("d","Tipo de item não encontrado!");
-  redirect('tipos_equipamento.php');
+  redirect('tipos_itens.php');
 }
 
-if(isset($_POST['edit_type_equip'])){
-  $req_field = array('type_equip-name');
-  $type_equip_name = remove_junk($db->escape($_POST['type_equip-name']));
+if(isset($_POST['edit_types_item'])){
+  $req_field = array('item-name');
+  $t_item_name = remove_junk($db->escape($_POST['item-name']));
   if(empty($errors)){
-    $sql = "UPDATE types_equips SET name='{$type_equip_name}'";
-    $sql .= " WHERE id='{$type_equip['id']}'";
+    $sql = "UPDATE types_itens SET name='{$t_item_name}'";
+    $sql .= " WHERE id='{$types_item['id']}'";
     $result = $db->query($sql);
     if($result && $db->affected_rows() === 1) {
       $session->msg("s", "Tipo de item alterado com sucesso");
-      redirect('tipos_equipamento.php',false);
+      redirect('tipos_itens.php',false);
     } else {
       $session->msg("d", "Desculpe, falha ao alterar o tipo de item.");
-      redirect('tipos_equipamento.php',false);
+      redirect('tipos_itens.php',false);
     }
   } else {
     $session->msg("d", $errors);
-    redirect('tipos_equipamento.php',false);
+    redirect('tipos_itens.php',false);
   }
 }
 ?>
@@ -43,15 +43,15 @@ if(isset($_POST['edit_type_equip'])){
       <div class="panel-heading">
         <strong>
           <span class="glyphicon glyphicon-edit"></span>
-          <span>Editando <?= remove_junk(ucfirst($type_equip['name']));?></span>
+          <span>Editando <?= remove_junk(ucfirst($types_item['name']));?></span>
         </strong>
       </div>
       <div class="panel-body">
-        <form method="post" action="editar_tipos_itens.php?id=<?= (int)$type_equip['id'];?>">
+        <form method="post" action="editar_tipos_itens.php?id=<?= (int)$types_item['id'];?>">
           <div class="form-group">
-            <input type="text" class="form-control" name="type_equip-name" required autocomplete="off" value="<?= remove_junk(ucfirst($type_equip['name']));?>">
+            <input type="text" class="form-control" name="item-name" required autocomplete="off" value="<?= remove_junk(ucfirst($types_item['name']));?>">
           </div>
-          <button type="submit" name="edit_type_equip" class="btn btn-success">Atualizar</button>
+          <button type="submit" name="edit_types_item" class="btn btn-success">Atualizar</button>
         </form>
       </div>
     </div>
